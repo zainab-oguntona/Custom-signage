e64b745e31897b7804ad29ff22de55ed1f8dadd1
Victor <victoluolatunji@gmail.com>
2024-08-13 22:54:14 +0100

    tried to fix

--- script.js
+++ script.js
@@ -1,44 +1,74 @@
-"use strict";
+// "use strict";
 
-let showH = document.querySelector(".spn");
-// let nameEntered = "";
-// const costPerLetter = Number(5);
+function calculateCost() {
+  // SELECT ALL TE NEEDED ELEMENTS
+  const nameInput = document.getElementById("nameInput").value.trim();
 
-// function to calculate cost
-const calculateCost = function (message, costPerLetter) {
-  const numberOfLetters = message.length;
-  const totalCost = numberOfLetters * costPerLetter;
-  return totalCost;
-};
-let totalCost;
-document.querySelector(".btn").addEventListener("click", function () {
-  const search = document.querySelector(".search").value;
+  const resultDiv = document.getElementById("result");
+  const errorDiv = document.getElementById("error");
 
-  // when is there is no input
-  if (!search) {
-    showH.classList.remove("spn");
-    document.querySelector(".search").style.border = "1px solid red";
-    // when a name is inputed
-  } else if (search) {
-    const newCost = function () {
-      totalCost = calculateCost(search, Number(5));
-    };
-    console.log(`The total cost is ${totalCost}`);
+  // console.log(errorDiv);
+
+  // Clear previous results and errors
+  resultDiv.innerHTML = "";
+  // errorDiv.innerHTML = "";
+
+  // Check if the user entered a value
+  if (nameInput === "") {
+    errorDiv.innerHTML = "Please enter a name below...";
+    console.log("yooo, caant be empty");
+    return;
   }
 
-  // when a name is inputed ...  First trial
-  // } else if (search === nameEntered) {
-  //   const calculateCost = function () {
-  //     // get the numer of letter in the input
-  //     const numberOfLetters = nameEntered.length;
-  //     // multiply by the cost per letter
-  //     const totalCost = numberOfLetters * costPerLetter;
+  // Calculate the cost
+  const costPerLetter = 5;
+  const nameLength = nameInput.length;
+  const totalCost = nameLength * costPerLetter;
 
-  //     return totalCost;
+  // Show the result
+  resultDiv.innerHTML = `<p>${nameInput
+    .split("")
+    .join(" ")}</p><div class="cost">$${totalCost}</div>`;
+}
 
-  //   };
-  //   calculateCost();
-  // console.log(`The total cost is ${nameEntered}`);
+// let showH = document.querySelector(".spn");
+// // let nameEntered = "";
+// // const costPerLetter = Number(5);
 
-  console.log(value);
-});
+// // function to calculate cost
+// const calculateCost = function (message, costPerLetter) {
+//   const numberOfLetters = message.length;
+//   const totalCost = numberOfLetters * costPerLetter;
+//   return totalCost;
+// };
+// let totalCost;
+// document.querySelector(".btn").addEventListener("click", function () {
+//   const search = document.querySelector(".search").value.trim();
+
+//   // when is there is no input
+//   if (!search) {
+//     showH.classList.remove("spn");
+//     document.querySelector(".search").style.border = "1px solid red";
+//     // when a name is inputed
+//   } else if (search) {
+//     const newCost = function () {
+//       totalCost = calculateCost(search, Number(5));
+//       return totalCost;
+//     };
+//     console.log(`The total cost is ${newCost}`);
+//   }
+
+//   // when a name is inputed ...  First trial
+//   // } else if (search === nameEntered) {
+//   //   const calculateCost = function () {
+//   //     // get the numer of letter in the input
+//   //     const numberOfLetters = nameEntered.length;
+//   //     // multiply by the cost per letter
+//   //     const totalCost = numberOfLetters * costPerLetter;
+
+//   //     return totalCost;
+
+//   //   };
+//   //   calculateCost();
+//   // console.log(`The total cost is ${nameEntered}`);
+// });
