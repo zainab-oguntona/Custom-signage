321695c523a7c1777893b4b639d59b8ac3384b37
zainab-oguntona <152652838+zainab-oguntona@users.noreply.github.com>
2024-08-13 22:03:45 +1200

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let showH = document.querySelector('.spn');
+// let nameEntered = "";
+// const costPerLetter = Number(5);
+
+// function to calculate cost
+const calculateCost = function (message, costPerLetter) {
+  const numberOfLetters = message.length;
+  const totalCost = numberOfLetters * costPerLetter;
+  return totalCost;
+}
+let totalCost;
+document.querySelector('.btn').addEventListener('click', function () {
+  const search = document.querySelector('.search').value;
+
+  // when is there is no input
+  if (!search) {
+    showH.classList.remove('spn')
+    document.querySelector('.search').style.border = '1px solid red'
+    // when a name is inputed
+  } else if (search) {
+    const newCost = function () {
+      totalCost = calculateCost(search, Number(5));
+    }
+    console.log(`The total cost is ${totalCost}`)
+  }
+
+
+
+
+
+  // when a name is inputed ...  First trial
+  // } else if (search === nameEntered) {
+  //   const calculateCost = function () {
+  //     // get the numer of letter in the input
+  //     const numberOfLetters = nameEntered.length;
+  //     // multiply by the cost per letter
+  //     const totalCost = numberOfLetters * costPerLetter;
+
+  //     return totalCost;
+
+  //   };
+  //   calculateCost();
+  // console.log(`The total cost is ${nameEntered}`);
+
+
+});
+
